7d1880bd35dd3cb1f0ec07599dff9c1601ac59ff
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-04-05 11:59:50 -0700

    WIP isolating last index of roundscore
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>

--- README.md
+++ README.md
@@ -7,3 +7,37 @@ Expected Output: 1
 Test: "It should return a number from an array of numbers 1-6."
 Code: Roll(0);
 Expected Output: 1 
+
+Test: "It should return a random number from an array of 1-6."
+Code: Roll();
+Expected Output: ANY NUMBER 1-6
+
+Test: "It should push the result of the function into an array."
+Code: Roll();
+Expected Output: [N, N, N...]
+
+Test: "It should sum the total of the rolls from the array"
+Code: Roll();
+Expected Output: 18
+
+Test: "It should sum the total of the rolls from the array, but if a 1 is rolled, return a round total of 0"
+Code: Roll();
+Expected Output: 0
+
+
+//
+Describe: Player()
+
+Test: "It should create an instance of a new player object"
+Code: let player1 = new Player("Jimmy");
+Expected Output: Player {name: 'Jimmy', rounds: Array(0), score: 0}
+
+
+//
+Describe: hold()
+
+Test: "It should create a method to push a number into a player's rounds key"
+Code: player1.hold(1);
+player1;
+Expected Output: Player {name: 'Jimmy', rounds: Array(1), score: 0}
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,52 @@
 // Business Logic
 
-let totalArray = [];
-const diceArray = [1, 2, 3, 4, 5, 6];
-function roll() {
-	let die = diceArray[Math.floor(Math.random() * diceArray.length)];
-  totalArray.push(die);
-	let roundTotal = 0;
-	for (let i = 0; i < totalArray.length ; i ++) {
-  roundTotal = roundTotal + totalArray[i];
-	console.log(roundTotal);
-	}
+function Player(name) {
+	this.name = name;
+	this.rounds = [];
+	this.roundScore = 0;
+	this.totalScore = 0;
 }
 
+Player.prototype.hold = function(roundTotal) {
+	this.rounds.push(roundTotal);
+};
 
+Player.prototype.scoreboard = function(roundSum) {
+	this.roundScore = this.roundScore + roundSum
+}
+
+let totalArray = [];
+Player.prototype.roll = function() {
+	const diceArray = [1, 2, 3, 4, 5, 6];
+	let die = diceArray[Math.floor(Math.random() * diceArray.length)];
+	if (die !== 1) {
+		totalArray.push(die);
+		let roundTotal = 0;
+		for (let i = 0; i < totalArray.length; i ++) {
+		roundTotal = roundTotal + totalArray[i];
+		console.log(die);
+		console.log(totalArray);
+		console.log(roundTotal);
+		
+		}
+		this.hold(roundTotal);
+		console.log(this.rounds[this.rounds.length]);
+		this.roundScore = this.rounds[this.rounds.length];
+		// let roundSum = 0;
+		// roundSum = this.rounds.pop();
+		// this.scoreboard(roundSum);
+
+	} else {
+		// return roundTotal;
+		totalArray = [];
+		// console.log(die);
+		// console.log(0);
+		this.hold(0);
+	}
+};
+
+
+
+
+
+let player1 = new Player("Jimmy");
