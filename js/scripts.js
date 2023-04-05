c9f861f6b13f80dc2f7ad606fe658c714dc1d676
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-04-05 09:37:26 -0700

    add round total sum
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 // Business Logic
 
-function roll(dice) {
-	let die = diceArray[dice]
-  return parseInt(die);
-
-}
+let totalArray = [];
 const diceArray = [1, 2, 3, 4, 5, 6];
+function roll() {
+	let die = diceArray[Math.floor(Math.random() * diceArray.length)];
+  totalArray.push(die);
+	let roundTotal = 0;
+	for (let i = 0; i < totalArray.length ; i ++) {
+  roundTotal = roundTotal + totalArray[i];
+	console.log(roundTotal);
+	}
+}
+
+
