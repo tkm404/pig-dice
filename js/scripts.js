66d0edc10dc9453fcaf6954f8d37f0ec36cbca0b
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-04-05 17:01:24 -0700

    WIP summing total score of multiple rounds
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,6 @@ function Player(name, turn) {
 	this.roundScore = 0;
 	this.roundTotals = [];
 	this.totalScore = 0;
-	this.hit = true;
 	this.turn = turn;
 }
 
@@ -22,35 +21,49 @@ Player.prototype.collect = function(roundTotal) {
 	this.rounds.push(roundTotal);
 };
 
-Player.prototype.stay = function(roundScore) {
-	this.roundScore = this.rounds[this.rounds.length-1];
-	this.turn = false;
+// Player.prototype.stay = function(roundScore) {
+// 	this.roundScore = this.rounds[this.rounds.length-1];
+// 	this.turn = false;
+// }
+
+// Player.prototype.scoreboard = function(roundSum) {
+// 	this.roundScore = this.roundScore + roundSum
+// }
+
+
+Player.prototype.endTurn = function() {
+	this.roundTotals.push(this.roundScore);
+	for (let i = 0; i < this.roundTotals.length; i ++) {
+		this.totalScore += this.roundTotals[i];
+	}
+	if (this.turn === true) {
+		turnBoolean = false;
+		} else if (this.turn === false) {
+			turnBoolean = true;
+		}
+	roundArray = [];
 }
 
-Player.prototype.scoreboard = function(roundSum) {
-	this.roundScore = this.roundScore + roundSum
-}
-
-let totalArray = [];
+let roundArray = [];
 let turnBoolean = true;
 Player.prototype.roll = function() {
 	if (this.turn === turnBoolean) {
 		const diceArray = [1, 2, 3, 4, 5, 6];
 		let die = diceArray[Math.floor(Math.random() * diceArray.length)];
 		if (die !== 1) {
-			totalArray.push(die);
+			roundArray.push(die);
 			let roundTotal = 0;
-			for (let i = 0; i < totalArray.length; i ++) {
-			roundTotal = roundTotal + totalArray[i];
-			console.log(die);
-			console.log(totalArray);
-			console.log(roundTotal);
+			for (let i = 0; i < roundArray.length; i ++) {
+			roundTotal = roundTotal + roundArray[i];
+			// console.log(die);
+			// console.log(roundArray);
+			// console.log(roundTotal);
 			}
 			this.collect(roundTotal);
 			this.roundScore = this.rounds[this.rounds.length-1];
-			this.roundTotals.push(this.roundScore);
+			// this.roundTotals.push(this.roundScore);
 		}	else {
-			totalArray = [];
+			roundArray = [];
 			this.collect(0);
 			this.roundScore = this.rounds[this.rounds.length-1];
 			this.roundTotals.push(this.roundScore);
